56464fc362feef98ede38f9cd5bf700f526f374e
valeriaconstanzabc <valeria.brito.c@gmail.com>
2020-07-27 16:01:28 -0400

    función buscar y filtrar archivos .md

---
+++ funtions.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+
+const read = () => {
+  // const ruta = process.argv[2]
+  const ruta = __dirname;
+
+  fs.readdir(ruta, (error, files) => {
+
+    files.forEach(archivo => {
+      if(archivo.includes('.md')){
+          console.log(archivo)
+      }
+    })
+  })
+}
+
+module.exports.read = read()
+
+
+// module.exports = () => {
+
+// };

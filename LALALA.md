8a8107fc9363f6c862920f3b11485f924a9321e8
valeriaconstanzabc <valeria.brito.c@gmail.com>
2020-08-02 18:50:12 -0400

    agrego libreria de iconos

--- LALALA.md
+++ LALALA.md
@@ -3,3 +3,5 @@ Hola buenos días
 [Esto es un link ](https://github.com/Laboratoria/SCL013-md-links)
 [LALALA](https://nodejs.org/)
 [LALALA](https://developers.google.com/v8/)
+[Link malo](https://gist.github.com/rxjjaviers/7360908)
+[Link prueba http](http://gist.github.com/rxjjaviers/7360908)
--- prueba2.js
+++ prueba2.js
@@ -3,6 +3,7 @@ const pathNPM = require('path');
 const marked = require("marked");
 const fetch = require('node-fetch');
 const chalk = require('chalk');
+const emoji = require('node-emoji')
 
 //<-------------------CREAMOS RUTA ABSOLUTA-------......------>
 let path = __dirname;
@@ -29,62 +30,70 @@ const filterLinks = (links) => {
 const readDir = () => {
 
   return new Promise ((resolve, reject) => {
+    //------leyendo directorio------------------------------->
     fs.readdir(path, (err, files) => {
 
       if(err){
-        reject('ERROR', err)
+        reject('ERROR - No se puede leer el directorio', err)
       }
       else {
+        console.log('--------------------------------------------------------------')
         files.map(file => {
           if(pathNPM.extname(file) === ".md"){
-            console.log('Archivo leído: ', file)
+            console.log((emoji.get('exclamation')),'Archivo leído: ', file)
+            console.log('--------------------------------------------------------------')
 
+            //-----leyendo archivos del directorio leído------>
             fs.readFile(file, 'utf-8', (err, data) => {
 
-                if(err){
-                  console.log('ERROR', err)
+              if(err){
+                console.log('ERROR - No se puede leer el archivo', err)
+              }
+              else {
+                let links = []
+                const renderer = new marked.Renderer()
+
+                renderer.link = (href, title, text) => {
+                  links.push({
+                    Link:href,
+                    Titulo:text,
+                    Ruta:path,
+                  })
                 }
-                else {
-                  if(data.includes('http')){
-                    let links = []
-                    const renderer = new marked.Renderer()
 
-                    renderer.link = (href, title, text) => {
-                      links.push({
-                        Link:href,
-                        Titulo:text,
-                        Ruta:path,
-                      })
-                    }
+                marked(data, { renderer : renderer })
+                let resultLinks = filterLinks(links);
 
-                    marked(data, { renderer : renderer })
-                    let resultLinks = filterLinks(links);
-
-                    resultLinks.map((element) => {
-                      fetch(element.Link)
-                        .then(res => {
-                          if(res.status == 200) {
-                            console.log(
-                              chalk.green('[✔]'),
-                              chalk.cyan(element.Link),
-                              chalk.bgGray(` ${res.status} ${res.statusText} `))
-                          }
-                          else {
-                            console.log(
-                              chalk.red('[X]'),
-                              chalk.cyan(element.Link),
-                              chalk.bgRed(` ${res.status} ${res.statusText} `))
-                          }
-                        })
-                        .catch((err) => {
-                          console.log(
-                            chalk.gray('[-]'),
-                            chalk.cyan(element.Link),
-                            chalk.bgRed(` ${err.type} ${err.code} `))
-                        })
+                resultLinks.map((elem) => {
+                  fetch(elem.Link)
+                    .then(res => {
+                      if(res.status == 200) {
+                        console.log(
+                          chalk.green((emoji.get('heavy_check_mark')), ' '),
+                          chalk.cyan(elem.Link),
+                          chalk.green(`${'['}${file}${']'}`),
+                          chalk.black.bgGreen(` ${'Link bueno buenito'} `)
+                        )
+                      }
+                      else {
+                        console.log(
+                          chalk.red((emoji.get('x')), ''),
+                          chalk.red(elem.Link),
+                          chalk.black.red(`${'['}${file}${']'}`),
+                          chalk.black.bgRed(` ${'ERROR 404 - link dañado'} `)
+                        )
+                      }
                     })
-                  }
-                }
+                    .catch((err) => {
+                      console.log(
+                        chalk.gray((emoji.get('no_entry')), ' '),
+                        chalk.cyan(elem.Link),
+                        chalk.redBright(`${'['}${file}${']'}`),
+                        chalk.black.bgRed(` ${'Error de conexión. Vuelve a intentarlo más tarde '} `)
+                      )
+                    })
+                })
+              }
             })
           }
         })
@@ -93,18 +102,5 @@ const readDir = () => {
   })
 }
 
-
-//<----------------FUNCIÓN QUE CONSUME PROMESA---------------->
-const promise = (err, data) => {
-  Promise.all([readDir()])
-  .then(()=> {
-    console.log(data)
-  })
-  .catch(() => {
-    console.log(err);
-  })
-}
-
-
 //<----------------EXPORTAMOS MÓDULO PROMESA------------------>
-module.exports.promise = promise();
+module.exports.readDir = readDir();
